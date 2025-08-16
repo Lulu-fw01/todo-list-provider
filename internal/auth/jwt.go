e16470578907764ab9b9e6ebe3918b145b80c50a
luka.markov <luka.markov@aliexpress.ru>
2025-08-16 15:56:52 +0300

    Implement JWT authentication service with middleware; refactor CreateJWT function to use configuration settings for token generation and validation

--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -1,27 +1,90 @@
 package auth
 
 import (
-	// "github.com/golang-jwt/jwt"
+	"fmt"
+	"net/http"
 	"strconv"
 	"time"
+	"todo-list-provider/configs"
 
+	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
 )
 
-func CreateJWT(secret []byte, userId int) (string, error) {
-	// todo use value from config
-	expiration := time.Hour * 24
+const (
+	authHeader = "Authorization"
+	userIDKey  = "userID"
+)
+
+type AuthServcie struct {
+	cfg *configs.AuthConfig
+}
+
+func NewAuthService(cfg *configs.AuthConfig) AuthServcie {
+	return AuthServcie{
+		cfg: cfg,
+	}
+}
+
+func (s *AuthServcie) CreateJWT(userId int) (string, error) {
+	expiration := time.Second * time.Duration(s.cfg.JwtExperationSeconds)
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"userID":    strconv.Itoa(userId),
+		userIDKey:   strconv.Itoa(userId),
 		"expiredAt": time.Now().Add(expiration),
 	})
 
-	tokenStr, err := token.SignedString(secret)
+	tokenStr, err := token.SignedString([]byte(s.cfg.JwtSecretPhrase))
 
 	if err != nil {
 		return "", err
 	}
 
 	return tokenStr, nil
+}
+
+func (s *AuthServcie) JWTAuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenStr := c.GetHeader(authHeader)
+		if tokenStr == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			c.Abort()
+			return
+		}
+
+		token, err := jwt.Parse(tokenStr, func(t *jwt.Token) (interface{}, error) {
+			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("")
+			}
+			return []byte(s.cfg.JwtSecretPhrase), nil
+		})
+
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			c.Abort()
+			return
+		}
+
+		if !token.Valid {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			c.Abort()
+			return
+		}
+
+		payload := token.Claims.(jwt.MapClaims)
+		userIDStr := payload[userIDKey].(string)
+		userId, _ := strconv.Atoi(userIDStr)
+		// todo check user exist
+
+		c.Set(userIDKey, userId)
+		c.Next()
+	}
+}
+
+func GetUserIDFromCtx(c *gin.Context) int {
+	val, exist := c.Get(userIDKey)
+	if !exist {
+		return -1
+	}
+	return val.(int)
 }
